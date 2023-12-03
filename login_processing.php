e5bb83b55c83893d1c5c99c8bd9d94ef1c19602e
TranTriDat2002 <trantridat2002@gmail.com>
2023-12-04 04:00:02 +0700

    Update login_processing.php
    Done!!???

--- login_processing.php
+++ login_processing.php
@@ -19,11 +19,12 @@ session_start();
     if (isset($_POST['username']) && isset($_POST['password'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
-
-
-        $sql = "SELECT id, userName, password FROM users WHERE username = '$username'";
-        $result = $conn->query($sql);
-        // SQL Injection!!!
+        
+        $sql = "SELECT id, userName, password FROM users WHERE username = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
         $user = $result->fetch_assoc();
 
         if ($result->num_rows == 1) {
