1075c97fb0f4a7a9bd4ba4c3a02ad6d0b320944a
Dat <90046310+TangTuanDat@users.noreply.github.com>
2023-12-03 23:15:01 +0700

    add template to fetch data

--- src/generate-pdf.php
+++ src/generate-pdf.php
@@ -15,47 +15,48 @@ $dompdf = new Dompdf($options);
 
 $dompdf->setPaper("A4", "portrait");
 
-$sql = file_get_contents("../database/sqlstatement.sql");
+// $sql = file_get_contents("../database/sqlstatement.sql");
+$sql = "select 
+company_name as companyName,
+position as companyPosition,
+duration as companyTime,
+tasks as companyDescription
+from working_history where user_id = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $_GET["id"]);
 $stmt->execute();
 $result = $stmt->get_result();
 
-$data = $result->fetch_assoc();
-foreach ($data['university'] as ) {
-    if (is_string($uni)) {
-        echo "yes";
-    } else {
-        echo "no";
-    }
+$experiences = [];
+while ($row = $result->fetch_assoc()) {
+    $experiences[] = $row;
 }
-// var_dump($data['university']);
 
-// $html = file_get_contents("harvardCVtemplate.html");
+$html = file_get_contents("harvardCVtemplate.html");
 
-// $html = str_replace(
-//     [
+$html = str_replace(
+    [
 //         "{{ displayName }}",
 //         "{{ displayEmail }}",
 //         "{{ displayPhone }}",
 //         "{{ displayUniversityInfo }}",
 //         "{{ displayTechSkills }}",
 //         "{{ displayCertifications }}",
-//         "{{ displayExperiences }}"
-//     ],
-//     [
+        "{{ displayExperiences }}"
+    ],
+    [
 //         displayName($decodedData),
 //         displayEmail($decodedData),
 //         displayPhone($decodedData),
 //         displayUniversityInfo(json_decode($decodedData['university'], true)),
 //         displayTechSkills(json_decode($decodedData['techSkills'], true)),
 //         displayCertifications(json_decode($decodedData['certificates'], true)),
-//         displayExperiences(json_decode($decodedData['experiences'], true))
-//     ],
-//     $html
-// );
+        displayExperiences($experiences)
+    ],
+    $html
+);
 
-// $dompdf->loadHtml($html);
+$dompdf->loadHtml($html);
 
-// $dompdf->render();
-// $dompdf->stream("Curriculum Vitae.pdf", ["Attachment" => 0]);
+$dompdf->render();
+$dompdf->stream("Curriculum Vitae.pdf", ["Attachment" => 0]);
